9e48bea0d47dd777b0fe57d02dab605ff6b119ab
Alberto Jaen <alberto.jaen@olympicchannel.com>
2021-12-22 20:33:07 +0100

    Deleted unneeded taints

--- scripts/reset_tokens.sh
+++ scripts/reset_tokens.sh
@@ -1,3 +1,1 @@
-terraform taint module.databricks_provisioning.databricks_token.pat
-terraform taint module.databricks_provisioning.databricks_obo_token.this
 terraform apply -var-file="vars/databricks.tfvars" -target=module.databricks_provisioning
