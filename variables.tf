eda98767c4277de94f5cc987c9d9597969d4ce2c
Alberto Jaen <alberto.jaen@olympicchannel.com>
2022-01-10 13:24:10 +0100

    Added descriptions to variables

--- variables.tf
+++ variables.tf
@@ -7,6 +7,7 @@ variable "workspace_name" {
 }
 
 variable "customer_managed_vpc" {
+  description = "configuration for the vpc that will be deployed to host the Databricks cluster"
   type = any
 }
 
@@ -26,49 +27,61 @@ variable "prefix" {
 }
 
 variable "tags" {
+  description = "tags that will be added to all the resources created"
   type = map(string)
 }
 
 variable "aws_datalake_kms" {
+  description = "configuration for the encryption key used to encrypt the datalake buckets"
   type = map(string)
 }
 
 variable "aws_baseline_kms" {
+  description = "encryption key used to encrypt logging buckets"
   type = map(string)
 }
 
 variable "aws_baseline_s3_logging" {
+  description = "configuration for the logging buckets (vpc flow logs, etc...)"
   type = any
 }
 
 variable "aws_databricks_logs_s3" {
+  description = "configuration for the Databricks logging buckets (cluster, billing and audit logs)"
   type = any
 }
 
 variable "aws_root_s3" {
+  description = "configuration for the root bucket"
   type = any
 }
 
 variable "aws_datalake_s3_raw" {
+  description = "configuration for the datalake raw bucket"
   type = any
 }
 
 variable "aws_datalake_s3_prepared" {
+  description = "configuration for the datalake prepared bucket"
   type = any
 }
 
 variable "aws_datalake_s3_trusted" {
+  description = "configuration for the datalake trusted bucket"
   type = any
 }
 
 variable "cluster_config" {
+  description = "configuration for the Databricks cluster"
   type = any
 }
 
 variable "dbfs_client_side_enc" {
+  description = "set to true to enable client side encryption on the DBFS"
   type = string
 }
 
 variable "datalake_client_side_enc" {
+  description = "set to true to enable client side encryption on the datalake buckets (raw, prepared and trusted)"
   type = string
 }
